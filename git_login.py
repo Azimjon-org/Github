4da017d08cd84b8e0012e5167427a142fb12ef1f
Azimjon-org <tonym5one@gmail.com>
2024-06-12 11:19:43 +0500

    Initial commit

---
+++ git_login.py
@@ -0,0 +1,23 @@
+""" """
+
+"""
+
+git config --global user.name "Azimjon-org"
+git config --global user.email "tonym5one@gmail.com"
+
+
+        SSH -> generate
+    ssh-keygen -t rsa
+    cat ~/.ssh/id_rsa.pub
+
+
+
+
+
+         Git ignore  -> files
+    Python
+    pycharm -» global templates , JetBrains
+    venv -» global templates , Virtualnv
+         Qolda koshihs kerkalar:
+    .venv/
+"""
